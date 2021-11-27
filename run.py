4f025dcd32d27900b8e891867946ea7e53dd78ef
Olivertrain1221 <olivertrain1996@gmail.com>
2021-11-27 13:08:53 +0000

    Add basic startup of game logic

--- run.py
+++ run.py
@@ -1,3 +1,66 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def startup():
+    """
+    Game starts with message and options
+    """
+    print("Welcome to the Aston Martin Quiz\n")
+    print("""
+                                 _..-------++._
+                             _.-'/ |      _||  \"--._
+                       __.--'`._/_\j_____/_||___\    `----.
+                  _.--'_____    |          \     _____    /
+                _j    /,---.\   |        =o |   /,---.\   |_
+               [__]==// .-. \\==`===========/==// .-. \\=[__]
+                 `-._|\ `-' /|___\_________/___|\ `-' /|_.'
+                       `---'                     `---'""")
+
+def user_main_menu():
+    print("Are you ready to play?")
+    print("""
+    -          Play           -
+    -          Rules          -
+    -          Quit           -
+    Type 'P' for play, 'R' for rules or 'Q' for quit.""")
+    # main_menu_selection()
+
+
+
+def main_menu_selection():
+    """
+    Checking what the user inputs
+    """
+    option = ''
+    while option not in ['P', 'R', 'Q']:
+        
+        if option == 'P':
+            game_play()
+            break
+        elif option == 'R':
+            rule_options()
+            break
+        elif option == 'Q':
+            startup()
+        else:
+            print('\nHmmm you didnt follow the rules!!! please try again with valid input. So once again Please type "P" to play, "R" to remind the rules or "Q" to quit.')
+            continue
+
+
+def game_play():
+    """
+    Game startup of quiz
+    """
+
+
+def rule_options():
+    """
+    when selected will display rules
+    """
+    print("Welcome to the Aston Martin quiz, a quiz for all you buzzing petrol heads!\n")
+    print("In this game you will be asked a range of questions and the answers will need to be\n inputted but using the numbers on your keyboard '1, 2, 3'")
+
+
+def main():
+    startup()
+    user_main_menu()
+
+
+main()
