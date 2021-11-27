30e2a95758be594ca71778cef87b36209f539108
Olivertrain1221 <olivertrain1996@gmail.com>
2021-11-27 22:40:28 +0000

    Add start of dictionary

---
+++ questions.py
@@ -0,0 +1,193 @@
+html_questions = [
+    {
+        'question': 'What is the biggest naturally aspirated engine the Aston Martin sell?:',
+        'answers': ['V8', 'v12', '1.8'],
+        'correct': 'V12'
+    },
+    {
+        'question': '''You may remember in James bond there is a "special" branch
+         for customiseable anything:''',
+        'answers': ['Cue', 'Q', 'K'],
+        'correct': 'Q'
+    },
+    {
+        'question': '''The most iconic Aston Martin is:''',
+        'answers': ['Vantage', 'DB9', 'DB5'],
+        'correct': 'DB5'
+    },
+    {
+        'question': '''What is Aston Martins show stopper for safety:''',
+        'answers': ['There are extra airbags in the seats', 
+        'The bonnet air bags to cover the screen for reinforcement',
+        '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom pushing the car up and over engine to
+        decrease imapact to cockpit'''],
+        'correct': '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom oushing the car up and over engine to
+        decrease imapact to cockpit'''
+    },
+    {
+        'question': '''In the vanquish there is a option to have a square steering
+        wheel:''',
+        'answers': ['True',
+        '''False'''],
+        'correct': '''True'''
+    },
+    {
+        'question': '''What is Aston Martins new SUV:''',
+        'answers': ['Vantage',
+        'DB11',
+        'DBX'],
+        'correct': '''DBX'''
+    },
+    {
+        'question': '''What Is the smallest car aston martin have ever made:''',
+        'answers': ['Vantage',
+        'DB11',
+        'Cygnet'],
+        'correct': '''Cygent'''
+    },
+    {    
+        'question': '''There is currently 2 new engines which are AMG based what are these:''',
+        'answers': ['V6 twin turbo',
+        'V8 twin turbo',
+        'V12 twin turbo'],
+        'correct': '''V12 twin turbo'''
+    },
+    {    
+        'question': '''The DBS Superleggera has a 0.60 time of .... and has .... bhp:''',
+        'answers': ['3.4 715',
+        '4.4 550',
+        '3.8 650'],
+        'correct': '''3.4 715'''
+    },
+
+    html_questions = [
+    {
+        'question': 'What is the biggest naturally aspirated engine the Aston Martin sell?:',
+        'answers': ['V8', 'v12', '1.8'],
+        'correct': 'V12'
+    },
+    {
+        'question': '''You may remember in James bond there is a "special" branch
+         for customiseable anything:''',
+        'answers': ['Cue', 'Q', 'K'],
+        'correct': 'Q'
+    },
+    {
+        'question': '''The most iconic Aston Martin is:''',
+        'answers': ['Vantage', 'DB9', 'DB5'],
+        'correct': 'DB5'
+    },
+    {
+        'question': '''What is Aston Martins show stopper for safety:''',
+        'answers': ['There are extra airbags in the seats', 
+        'The bonnet air bags to cover the screen for reinforcement',
+        '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom pushing the car up and over engine to
+        decrease imapact to cockpit'''],
+        'correct': '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom oushing the car up and over engine to
+        decrease imapact to cockpit'''
+    },
+    {
+        'question': '''In the vanquish there is a option to have a square steering
+        wheel:''',
+        'answers': ['True',
+        '''False'''],
+        'correct': '''True'''
+    },
+    {
+        'question': '''What is Aston Martins new SUV:''',
+        'answers': ['Vantage',
+        'DB11',
+        'DBX'],
+        'correct': '''DBX'''
+    },
+    {
+        'question': '''What Is the smallest car aston martin have ever made:''',
+        'answers': ['Vantage',
+        'DB11',
+        'Cygnet'],
+        'correct': '''Cygent'''
+    },
+    {    
+        'question': '''There is currently 2 new engines which are AMG based what are these:''',
+        'answers': ['V6 twin turbo',
+        'V8 twin turbo',
+        'V12 twin turbo'],
+        'correct': '''V12 twin turbo'''
+    },
+    {    
+        'question': '''The DBS Superleggera has a 0.60 time of .... and has .... bhp:''',
+        'answers': ['3.4 715',
+        '4.4 550',
+        '3.8 650'],
+        'correct': '''3.4 715'''
+    },
+
+    ##
+
+    html_questions = [
+    {
+        'question': 'What is the biggest naturally aspirated engine the Aston Martin sell?:',
+        'answers': ['V8', 'v12', '1.8'],
+        'correct': 'V12'
+    },
+    {
+        'question': '''You may remember in James bond there is a "special" branch
+         for customiseable anything:''',
+        'answers': ['Cue', 'Q', 'K'],
+        'correct': 'Q'
+    },
+    {
+        'question': '''The most iconic Aston Martin is:''',
+        'answers': ['Vantage', 'DB9', 'DB5'],
+        'correct': 'DB5'
+    },
+    {
+        'question': '''What is Aston Martins show stopper for safety:''',
+        'answers': ['There are extra airbags in the seats', 
+        'The bonnet air bags to cover the screen for reinforcement',
+        '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom pushing the car up and over engine to
+        decrease imapact to cockpit'''],
+        'correct': '''The last and most extreme of 70mph crumple zone will drop the engine
+        V8 OR V12 out the bottom oushing the car up and over engine to
+        decrease imapact to cockpit'''
+    },
+    {
+        'question': '''In the vanquish there is a option to have a square steering
+        wheel:''',
+        'answers': ['True',
+        '''False'''],
+        'correct': '''True'''
+    },
+    {
+        'question': '''What is Aston Martins new SUV:''',
+        'answers': ['Vantage',
+        'DB11',
+        'DBX'],
+        'correct': '''DBX'''
+    },
+    {
+        'question': '''What Is the smallest car aston martin have ever made:''',
+        'answers': ['Vantage',
+        'DB11',
+        'Cygnet'],
+        'correct': '''Cygent'''
+    },
+    {    
+        'question': '''There is currently 2 new engines which are AMG based what are these:''',
+        'answers': ['V6 twin turbo',
+        'V8 twin turbo',
+        'V12 twin turbo'],
+        'correct': '''V12 twin turbo'''
+    },
+    {    
+        'question': '''The DBS Superleggera has a 0.60 time of .... and has .... bhp:''',
+        'answers': ['3.4 715',
+        '4.4 550',
+        '3.8 650'],
+        'correct': '''3.4 715'''
+    },
